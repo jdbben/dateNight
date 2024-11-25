b0802ebdb0df67e01fd127355f024601b5c589dd
jdbben <benrahmounjamaleddine@gmail.com>
2024-11-25 23:13:30 +0100

    change the button ui

--- src/components/Discreption.tsx
+++ src/components/Discreption.tsx
@@ -7,6 +7,19 @@ import Botton from "./Botton";
 
 const Discreption = () => {
   const [loading, setLoading] = useState<boolean>(false);
+  const func = () => {
+    setLoading(true);
+
+    signIn(undefined, { callbackUrl: "/configure/gameSetup" });
+
+    setLoading(false);
+  };
+  //   <button type="button" class="bg-indigo-500 ..." disabled>
+  //   <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
+  //     <!-- ... -->
+  //   </svg>
+  //   Processing...
+  // </button>
 
   return (
     <div className="flex flex-col lg:gap-2 lg:h-screen items-center justify-center gap-4 pt-[15vh] pb-[5vh] lg:pb-[40%] ">
@@ -22,13 +35,37 @@ const Discreption = () => {
           <p className="text-white"> {text.features}</p>
         </div>
       </div>
-      <Botton
-        Name="Start playing"
-        OnClick={() =>
-          signIn(undefined, { callbackUrl: "/configure/gameSetup" })
-        }
-        loading={loading}
-      />
+      {loading ? (
+        <>
+          <button type="button" className="bg-indigo-500 ..." disabled>
+            <svg
+              className={`animate-spin h-5 w-5 mr-3 `}
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8v8H4z"
+              ></path>
+            </svg>
+            Processing...
+          </button>
+        </>
+      ) : (
+        <>
+          <Botton Name="Start playing" OnClick={func} />
+        </>
+      )}
     </div>
   );
 };
