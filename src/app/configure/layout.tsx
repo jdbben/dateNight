ed3cc6688da31156a9bbc5f2dd7f5e776a049e14
jdbben <benrahmounjamaleddine@gmail.com>
2024-11-25 15:42:46 +0100

    all

--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -1,13 +1,20 @@
+"use client";
 import { getSession, SessionProvider } from "next-auth/react";
 
 import MaxWidthWrapper from "../../components/MaxWidthWrapper";
 import NavBar from "../../components/NavBar";
 import Theme from "../../components/Theme";
 import { ReactNode } from "react";
+import { Session } from "next-auth";
 
-const layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+interface LayoutProps {
+  children: Readonly<React.ReactNode>;
+  session: Session;
+}
+
+const layout = ({ children, session }: LayoutProps) => {
   return (
-    <SessionProvider>
+    <SessionProvider session={session}>
       <NavBar />
       <MaxWidthWrapper className="flex justify-center items-center h-screen">
         {children}
