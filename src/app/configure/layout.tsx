8460eafeed13e6abf55b16cba79f4b3c44d54981
jdbben <benrahmounjamaleddine@gmail.com>
2024-11-25 15:30:48 +0100

    fix

--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -4,8 +4,11 @@ import MaxWidthWrapper from "../../components/MaxWidthWrapper";
 import NavBar from "../../components/NavBar";
 import Theme from "../../components/Theme";
 import { ReactNode } from "react";
+interface LayoutProps {
+  children: ReactNode;
+}
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default function Layout({ children }: LayoutProps) {
   return (
     <SessionProvider>
       <NavBar />
